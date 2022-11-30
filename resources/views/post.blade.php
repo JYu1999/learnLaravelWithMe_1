12eb2cd436e980abe0d1f870a483712a97b36e2b
JYu1999 <jk29666338@gmail.com>
2022-11-30 14:03:17 +0800

    ep.1 sec2 1-3

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>My blog</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post">First post</h1></a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nesciunt, nobis. Amet at dicta dolorum expedita, explicabo fugit harum, illum nisi obcaecati odit omnis repellat, repellendus similique tempore veniam voluptatem?</p>
+</article>
+
+<a href="/">Go back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>My blog</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post">First post</h1></a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nesciunt, nobis. Amet at dicta dolorum expedita, explicabo fugit harum, illum nisi obcaecati odit omnis repellat, repellendus similique tempore veniam voluptatem?</p>
+</article>
+<article>
+    <h1><a href="/post">Second post</h1></a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nesciunt, nobis. Amet at dicta dolorum expedita, explicabo fugit harum, illum nisi obcaecati odit omnis repellat, repellendus similique tempore veniam voluptatem?</p>
+</article>
+<article>
+    <h1><a href="/post">Third post</h1></a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, at culpa deleniti dolorum, eligendi error et expedita fuga impedit laborum maiores nisi officiis pariatur perferendis possimus praesentium quidem sed sunt.</p>
+</article>
+</body>
+</html>
